4d6d5f6edaf5f3df225d3acc035625a8c94b4755
ebi3102 <ebi3102@gmail.com>
2023-08-07 06:47:42 +0330

    Add main Menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,22 @@
 'use client';
+
+import Image from "next/image";
+import Main_Menu from "./headers/main-menu";
+
 const Header = () => {
     return ( 
-        <div className="flex flex-col items-center justify-between p-24">
-            Header PRIMARY
+        <div className="flex items-center justify-between p-24">
+            <div className="emo-logo">
+            <Image
+                src="/images/logo.png"
+                width={100}
+                height={100}
+                alt="Iphonery online shop"
+                />
+            </div>
+            <div className="emo-login">
+                <Main_Menu />
+            </div>
         </div>
      );
 }
---
+++ components/headers/main-menu.jsx
@@ -0,0 +1,18 @@
+const Main_Menu = () => {
+    return ( 
+        <div className="menu-container">
+            <nav className="main-menu">
+                <ul className="flex items-center justify-between p-24">
+                    <li className="ml-2 mr-2">Home</li>
+                    <li className="ml-2 mr-2">Shop</li>
+                    <li className="ml-2 mr-2">Blog</li>
+                    <li className="ml-2 mr-2">COntact Us</li>
+                    <li className="ml-2 mr-2">About Us</li>
+                    <li className="ml-2 mr-2">Privacy Policy</li>
+                </ul>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Main_Menu;
